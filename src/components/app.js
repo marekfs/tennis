8fd8ddd240e1f29a935c2166ab203140475a7cd2
Marek Siekeirski <marekfs@ca.ibm.com>
2022-12-12 10:26:56 -0500

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import { h } from 'preact';
+
+const App = () => (
+	<div id="app">
+		<div class='flex mx-auto justify-center bg-blue-300 h-screen'>
+			<div class='column-1'>
+				<h1 class='font-bold text-2xl text-yellow-200'>Countdown to Tennis Season</h1>
+			</div>
+		</div>
+	</div>
+)
+
+export default App;
