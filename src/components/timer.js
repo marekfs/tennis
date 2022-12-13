5a1cd0e9176e7ed495cd1d2202fb32b2779a3d52
Marek Siekierski <mark.siekierski@gmail.com>
2022-12-13 01:05:27 -0500

    Fixed countdown timer

--- src/components/timer.js
+++ src/components/timer.js
@@ -11,10 +11,10 @@ const Timer = () => {
   const [seconds, setSeconds] = useState(ms.seconds)
 
   const createCountdown = (ms) => {
-    setSeconds(Math.floor((ms % 6e4) / 1000))
-    setMinutes(Math.floor((ms % 36e5) / 6e4))
-    setHours(Math.floor((ms % 36e6) / 6e4))
-    setDays(Math.floor(ms / (1000*60*60*24)))
+    setSeconds(((ms % 60000) / 1000).toFixed(0))
+    setMinutes((Math.floor(ms / 60000)) % 60)
+    setHours((Math.floor(ms / 3600000)) % 24)
+    setDays((Math.floor(ms / 86400000)))
     setMs(ms-1000)
   }
 
@@ -35,7 +35,7 @@ const Timer = () => {
         <h1 class='font-sans font-bold italic text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>
           Countdown to Tennis Season
         </h1>
-        <h1 class='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>{`${days}D ${hours}H ${ minutes}m${seconds}s`}</h1>
+        <h1 class='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>{`${days}D ${hours}H ${ minutes}m ${seconds}s`}</h1>
       </div>
     </div>
   )
