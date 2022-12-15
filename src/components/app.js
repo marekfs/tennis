554d1a17c61602d3ed425d16ba6092506c5a3849
Marek Siekeirski <marekfs@ca.ibm.com>
2022-12-15 11:36:42 -0500

    animate timer

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ const App = () => (
   <div id='app'>
     <div class='flex flex-col mx-auto justify-center bg-blue-900 h-screen px-5'>
       <div class='column-1'>
-        <h1 class='font-sans font-bold italic text-3xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>
+        <h1 class='font-sans font-bold italic text-3xl md:text-4xl lg:text-6xl text-yellow-200 text-center'>
           Countdown to Tennis Season
         </h1>
         <Timer />
--- src/components/timer.js
+++ src/components/timer.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'preact/hooks'
+import ReactTextTransition, { presets } from "react-text-transition"
 
 const Timer = () => {
   const tennisTime = new Date('2023-03-05T10:00:00.0000-05:00')
@@ -30,7 +31,41 @@ const Timer = () => {
   },[days, hours, minutes, ms, seconds])
 
   return(
-    <h1 class='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>{`${days}D ${hours}H ${ minutes}m ${seconds}s`}</h1>
+    //<h1 >{`${days}D ${hours}H ${ minutes}m ${seconds}s`}</h1>
+    <div class='flex justify-center gap-2 md:gap-3 lg:gap-4'>
+      <div class='flex gap-x-1 lg:gap-x-2'>
+      <ReactTextTransition
+        className='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'
+        children={days}
+        springConfig={presets.gentle}
+      />
+      <h1 class='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>days</h1>
+      </div>
+      <div class='flex gap-x-1 lg:gap-x-2'>
+      <ReactTextTransition
+        className='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'
+        children={hours}
+        springConfig={presets.gentle}
+      />
+      <h1 class='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>h</h1>
+      </div>
+      <div class='flex gap-x-1 lg:gap-x-2'>
+      <ReactTextTransition
+        className='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'
+        children={minutes}
+        springConfig={presets.gentle}
+      />
+      <h1 class='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>m</h1>
+      </div>
+      <div class='flex gap-x-1 lg:gap-x-2'>
+      <ReactTextTransition
+        className='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'
+        children={seconds}
+        springConfig={presets.gentle}
+      />
+      <h1 class='font-mono text-2xl md:text-4xl lg:text-6xl text-yellow-200 pt-10 text-center'>s</h1>
+      </div>
+    </div>
   )
 }
 
